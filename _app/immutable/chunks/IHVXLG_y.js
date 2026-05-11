7e359f4661b10d1738798f9a56506de53983c193
jorgema21 <144554493+jorgema21@users.noreply.github.com>
2026-05-11 14:40:41 +0200

    Updates

---
+++ _app/immutable/chunks/IHVXLG_y.js
@@ -0,0 +1,1 @@
+import{J as o,x as t,m as c}from"./B4QPvFG2.js";function f(e){throw new Error("https://svelte.dev/e/lifecycle_outside_component")}function u(e){t===null&&f(),o(()=>{const n=c(e);if(typeof n=="function")return n})}export{u as o};
