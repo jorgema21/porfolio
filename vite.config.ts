28e163c4d0010fd1461b47b6dc5b54013bf25dbf
jorgema21 <144554493+jorgema21@users.noreply.github.com>
2026-05-21 17:41:32 +0200

    build: add automatic image optimization plugin to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		ViteImageOptimizer({
+			jpg: {
+				quality: 82,
+			},
+			jpeg: {
+				quality: 82,
+			},
+			png: {
+				compressionLevel: 9,
+				palette: true,
+			}
+		})
+	]
 });
