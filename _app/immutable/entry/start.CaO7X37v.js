e0e2646fe13ff0cfedd3d3dec1a2784082e8090c
jorgema21 <144554493+jorgema21@users.noreply.github.com>
2026-05-25 12:27:10 +0200

    Updates

--- _app/immutable/entry/start.CaO7X37v.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/C-7rVsks.js";import{y as t}from"../chunks/DJ9zhT3i.js";export{t as load_css,r as start};
---
+++ _app/immutable/entry/start.CoZgDwkK.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DtqESZ4e.js";import{y as t}from"../chunks/laM8IfWD.js";export{t as load_css,r as start};
